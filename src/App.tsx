b17168fa7be980839317d15ace3f8d0b3baa18c7
ruan.guangyao <545029283@qq.com>
2022-02-17 14:51:50 +0800

    feate 简单的登录demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
-import { ProjectListScreen } from "screens/project-list";
+// import { ProjectListScreen } from "screens/project-list";
+// import { CustomTest } from "custom-test/index";
+import { LoginScreen } from "screens/login";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      {/* <ProjectListScreen /> */}
+      {/* <CustomTest /> */}
+      <LoginScreen />
     </div>
   );
 }
---
+++ src/utils/index.ts
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
+
+//在一个函数里，改变传入的对象本身是不好的
+export const cleanObject = (object: object) => {
+  const result = {
+    ...object,
+  };
+  Object.keys(result).forEach((key) => {
+    //@ts-ignore
+    const value = result[key];
+    if (isFalsy(value)) {
+      //@ts-ignore
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+//custom hook
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+//custom debounce
+// export const useDebounceFunc = (func, delay) => {
+//     let timeOut;
+//     return (...param) => {
+//         if (timeOut) {
+//             clearTimeout(timeOut);
+//         }
+//         timeOut = setTimeout(() => {
+//             func(...param);
+//         }, delay);
+//     };
+// };
+
+export const useDebounce = <S>(value: S, delay?: number): S => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+//test
+export const useArray = (persons: { name: string; age: number }[]) => {
+  const [value, setValue] = useState(persons);
+
+  const add = (person: { name: string; age: number }): void => {
+    value.unshift(person);
+    const add_value = value;
+    setValue([...add_value]);
+  };
+
+  const removeIndex = (index: number): void => {
+    value.splice(0, 1);
+    const remove_value = value;
+    setValue([...remove_value]);
+  };
+
+  const clear = (): void => {
+    setValue([]);
+  };
+
+  return {
+    value,
+    clear,
+    removeIndex,
+    add,
+  };
+};
